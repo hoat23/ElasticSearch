200ff602a43d0d6277607f52a583c94e51769942
Deiner Zapata <deinerzapata@gmail.com>
2019-01-31 10:42:50 -0500

    Better error tracking.

--- bin/loadHeavyText2ELK.py
+++ bin/loadHeavyText2ELK.py
@@ -10,43 +10,60 @@ from utils import *
 import re
 import time
 import datetime
-
-nameHeavyFile = '/usr/share/logstash/padron_reducido_ruc.txt'
-char_sep = '|'
-
-block_size = 1E3
-cont = 0
-input_file = open(nameHeavyFile,'rb')
-print("[{0}] [START]".format(datetime.datetime.utcnow().isoformat() ) )
-block_data=[]
-elk = elasticsearch()
-while(1):
-    cont = cont + 1 
-    try:
-        line = input_file.readline()
-        line = line.decode('utf-8',errors='replace')
-        line = line.replace("\n","")
-        line = line.replace("\r","")
-        re.sub('[^a-zA-Z0-9-_*.]', '', line) #https://platzi.com/blog/expresiones-regulares-python/
-        
-        if(cont==1):
-            header_fields = line.split(char_sep)
-        else:
-            body_fields = line.split(char_sep)
-            data_json = list2json(header_fields, body_fields)
-            if(len(data_json)==0):
-                print("[{0}] {1}".format(datetime.datetime.utcnow().isoformat(),  cont))
-            data_json.update({'rename_index':'sunat',"source_id":cont})
-            #print_json(data_json)
-            #send_json(data_json,IP="54.208.72.130 ",PORT=5959)
-            #cont 308174  436693
-            block_data.append(data_json)
-        if(cont%block_size==0):
-            print("[{0}] [{1}]".format(datetime.datetime.utcnow().isoformat(), cont) )
-            elk.post_bulk(block_data,header_json={"index":{"_index":"sunat","_type":"_doc"}})
-            block_data=[]
-            time.sleep(1)
-    except:
-        print("[{0}] [ERROR | {1} ]".format(datetime.datetime.utcnow().isoformat(), cont) )
-        break
-print("[{0}] [ END ]".format(datetime.datetime.utcnow().isoformat()))
+import platform as p
+#########################################################################################
+if (p.system()=='Windows'):# ONLY WINDOWS
+    import winsound 
+#########################################################################################
+def beep_alert(f = 2500, t = 1000, count = 3):
+    if (p.system()=='Windows'):# ONLY WINDOWS
+        for i in range(0,count):
+            winsound.Beep(f,t)
+    return
+#########################################################################################
+def loadHeavyText2ELK(nameHeavyFile = 'padron_reducido_ruc.txt',char_sep = '|',block_size = 1E4,send_elk=False):
+    cont = 0
+    input_file = open(nameHeavyFile,'rb')
+    print("[{0}] [START]".format(datetime.datetime.utcnow().isoformat() ) )
+    block_data=[]
+    elk = elasticsearch()
+    sum_chars = 0
+    while(1):
+        cont = cont + 1 
+        try:
+            line = input_file.readline()
+            sum_chars =  sum_chars + len(line)
+            line = line.decode('utf-8',errors='replace')
+            line = line.replace("\n","")
+            line = line.replace("\r","")
+            re.sub('[^a-zA-Z0-9-_*.]', '', line) #https://platzi.com/blog/expresiones-regulares-python/
+            
+            if(cont==1):
+                header_fields = line.split(char_sep)
+            else:
+                data_json = {}
+                body_fields = line.split(char_sep)
+                data_json = list2json(header_fields, body_fields)
+                if(len(data_json)==0):
+                    print("[{0}] [ERROR | num line : {1} | num byte : {2}]".format(datetime.datetime.utcnow().isoformat(), cont, sum_chars) )
+                    beep_alert()
+                else:
+                    data_json.update({'rename_index':'sunat',"source_id":cont})
+                    #print_json(data_json)
+                    #send_json(data_json,IP="54.208.72.130 ",PORT=5959)
+                    #cont 308174  436693
+                    block_data.append(data_json)
+            if(cont%block_size==0):
+                if(send_elk):
+                    elk.post_bulk(block_data,header_json={"index":{"_index":"sunat","_type":"_doc"}})
+                else:
+                    print("[{0} | num line : {1}]".format(datetime.datetime.utcnow().isoformat(), cont) )
+                block_data=[]
+                time.sleep(1)
+        except:
+            print("[{0}] [ERROR | {1} ]".format(datetime.datetime.utcnow().isoformat(), cont) )
+            break
+    print("[{0}] [ END ]".format(datetime.datetime.utcnow().isoformat()))
+#########################################################################################
+if __name__ == "__main__":
+    loadHeavyText2ELK()
