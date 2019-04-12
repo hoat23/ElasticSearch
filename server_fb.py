21007e1af0aaa382b9eb93ccc76dc403963e923d
Deiner Zapata <deinerzapata@gmail.com>
2019-04-12 16:30:47 -0500

    Add files via upload

--- server_fb.py
+++ server_fb.py
@@ -8,7 +8,6 @@ from credentials import *
 from utils import print_json
 from datetime import datetime, timedelta
 from flask import Flask, request, abort
-from engine import *
 #######################################################################################
 host = "127.0.0.1"
 port = 8080
@@ -16,9 +15,19 @@ app = Flask(__name__)
 #########################################################################################
 def req_post(post_json , timeout=None):
     try: 
-        print("{0}|INFO | req_post | URL_API=[{1}]".format(datetime.datetime.utcnow().isoformat(), URL_API) )
+        URI_API = post_json['uri']
+        headers = post_json['qs']
+        data = post_json['json']
+        headers.update({'Content-Type': 'application/json'})
+        if type(data) != str : data = json.dumps(data)
+        rpt = requests.post(URL_API, headers=headers, data = data , timeout=timeout)
+        if not( (rpt.status_code)==200 or (rpt.status_code)==201 ):
+            print("[POST]:"+str(rpt.status_code)+" | "+ str(rpt.reason) )
+        
+
+        print("{0}|INFO | req_post | URL_API=[{1}]".format(datetime.utcnow().isoformat(), URL_API) )
     except:
-        print("{0}|ERROR| req_post | URL_API=[{1}]".format(datetime.datetime.utcnow().isoformat()) )
+        print("{0}|ERROR| req_post | URL_API=[{1}]".format(datetime.utcnow().isoformat()) )
     return
 #########################################################################################
 def callSendAPI(message ,sender_id="1610669258970681", type_msg = "RESPONSE" ):
@@ -31,7 +40,7 @@ def callSendAPI(message ,sender_id="1610669258970681", type_msg = "RESPONSE" ):
         "message": message
     }
 
-    print("{0}|INFO | post_fb | psid={1} type_msg={2}".format(datetime.datetime.utcnow().isoformat(), sender_id, type_msg) )
+    print("{0}|INFO | post_fb | psid={1} type_msg={2}".format(datetime.utcnow().isoformat(), sender_id, type_msg) )
     print_json(data_json)
     
     post_json = {
@@ -45,4 +54,18 @@ def callSendAPI(message ,sender_id="1610669258970681", type_msg = "RESPONSE" ):
     return
 
 
+"""
+curl -X POST -H "Content-Type: application/json" -d '
+{
+    "messaging_type": "RESPONSE",
+    "recipient": { 
+        "id": "1610669258970681"
+    },
+    "message": {
+        "text": "hello, world!"
+    } 
+}' "https://graph.facebook.com/v2.6/me/messages?access_token=EAACiYk1saZAgBAIc58EhTl2FiiWVEKmCmBcw52k4VDSVFumA6k76M5jy4njNAkWZA0H61AdC2tLa8UuLiPwBVdCATcHCKjGYr8M266WtWSqqP0c3FSV5ZBcXo6Yyf67bQI6JegoE2mMellhZATx76H0LZB6xasIATipP9wmnfQeDMSJ0 xAiYgY7Wgdz2ZBVjMZD"
 
+curl -X POST -H "Content-Type: application/json" -d '{"messaging_type": "RESPONSE","recipient": {"id": "1610669258970681"}, "message": {"text": "hello, world!"} }' "https://graph.facebook.com/v2.6/me/messages?access_token=EAACiYk1saZAgBAIc58EhTl2FiiWVEKmCmBcw52k4VDSVFumA6k76M5jy4njNAkWZA0H61AdC2tLa8UuLiPwBVdCATcHCKjGYr8M266WtWSqqP0c3FSV5ZBcXo6Yyf67bQI6JegoE2mMellhZATx76H0LZB6xasIATipP9wmnfQeDMSJ0 xAiYgY7Wgdz2ZBVjMZD"
+
+"""
