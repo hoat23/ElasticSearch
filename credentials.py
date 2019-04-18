1958f86c7ddc6605dbeaf043154fb13c650c4146
Deiner Zapata <deinerzapata@gmail.com>
2019-04-18 02:00:51 -0500

    Example of credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# Developer: Deiner Zapata Silva.
+# Date: 02/14/2019
+# Description: Credenciales de acceso y configuracion para configurar webhook con facebook.
+#########################################################################################
+VERIFY_TOKEN_FB = "dd3el11nce1ral1z1ana"
+ACCESS_TOKEN_FB = "Y7Wgdzsdfdfkjsdlfjlksdjflkdsjflksdlfhanvl2ZBVjMZD"
+
