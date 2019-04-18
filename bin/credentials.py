c8a90f55c281393ebcf36b02718e8b30a37552fa
Deiner Zapata <deinerzapata@gmail.com>
2019-04-18 04:02:38 -0500

    Update credentials.py

--- bin/credentials.py
+++ bin/credentials.py
@@ -5,4 +5,15 @@
 #########################################################################################
 VERIFY_TOKEN_FB = "dd3el11nce1ral1z1ana"
 ACCESS_TOKEN_FB = "Y7Wgdzsdfdfkjsdlfjlksdjflkdsjflksdlfhanvl2ZBVjMZD"
+SENDER_ID_H23 = "23232323232323" #sender_id para enviar en mensaje de messenger
+#########################################################################################
+URL = "https://1aSSSSSSSSSSSSSSSSSSSSSf.us-east-1.aws.found.io:9243" #key/webhooks
+USER = "USER"
+PASS = "PASSS"
+#########################################################################################
+HOST_SERVER = '127.0.0.1'
+PORT_SERVER = 4006
+#########################################################################################
+
+
 
