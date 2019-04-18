0832ddc41cdb604ebd7830cc18d2f968b0db1952
Deiner Zapata <deinerzapata@gmail.com>
2019-04-18 02:01:25 -0500

    Delete credentials.py

--- credentials.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-# Developer: Deiner Zapata Silva.
-# Date: 02/14/2019
-# Description: Credenciales de acceso y configuracion para configurar webhook con facebook.
-#########################################################################################
-VERIFY_TOKEN_FB = "dd3el11nce1ral1z1ana"
-ACCESS_TOKEN_FB = "Y7Wgdzsdfdfkjsdlfjlksdjflkdsjflksdlfhanvl2ZBVjMZD"
-
